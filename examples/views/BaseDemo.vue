3bd2f10a954b889209b16fb819f7f1ad8e5b09fe
vita2333 <lp_vitadolce@163.com>
2020-06-19 16:55:20 +0800

    fix: import VueAmap

--- examples/views/BaseDemo.vue
+++ examples/views/BaseDemo.vue
@@ -1,24 +1,18 @@
 <template>
-  <demo-block>
+  <div>
     <vue-amap></vue-amap>
-  </demo-block>
+  </div>
 </template>
 
 
 <script>
-  import Vue from 'vue'
   // import { VueAmap } from '../../packages'
   import { VueAmap } from '@vita2333/vue-amap2.0'
   import DemoBlock from '@/components/DemoBlock.vue'
 
-  console.log('vueAmap======================')
-  console.log(VueAmap) // todo
-  console.log('======================')
-
-  Vue.use(VueAmap)
   export default {
     name: 'BaseDemo',
-    components: { DemoBlock },
+    components: { DemoBlock, VueAmap },
   }
 </script>
 
