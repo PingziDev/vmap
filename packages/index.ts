8c280a8b181f1b88663d7b7c5bead3a0616fdbd3
vita2333 <lp_vitadolce@163.com>
2020-06-19 16:11:49 +0800

    fix: vue component not render

--- packages/index.ts
+++ packages/index.ts
@@ -6,7 +6,11 @@ import AMapLoader from '@amap/amap-jsapi-loader'
 const install: any = ((
   Vue1: VueConstructor<Vue>, options: AMapLoaderOptions) => {
   Vue1.prototype.$amapLoader = () => AMapLoader.load(options)
+  VueAmap.install(Vue1)
 })
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
 
 export { VueAmap }
 
